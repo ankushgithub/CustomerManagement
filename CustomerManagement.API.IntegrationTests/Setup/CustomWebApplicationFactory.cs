9d089351b47d9db8cc1afdd7690e9938f9dad3a7
Ankush Kashyap <ankushkashyap@gmail.com>
2022-02-21 12:36:39 +1100

    added setup

---
+++ CustomerManagement.API.IntegrationTests/Setup/CustomWebApplicationFactory.cs
@@ -0,0 +1,52 @@
+﻿using CustomerManagement.Db;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Net.Http;
+
+namespace CustomerManagement.API.IntegrationTests.Setup
+{
+    public class CustomWebApplicationFactory<TStartup>
+            : WebApplicationFactory<TStartup> where TStartup : class
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+
+                services.AddDbContext<CustomerManagementDbContext>(options =>
+                {
+                    options.UseInMemoryDatabase("CustomerDbInMemoryTest");
+                });
+
+                var sp = services.BuildServiceProvider();
+
+                using (var scope = sp.CreateScope())
+                {
+                    var scopedServices = scope.ServiceProvider;
+                    var context = scopedServices.GetRequiredService<CustomerManagementDbContext>();
+                    var logger = scopedServices.GetRequiredService<ILogger<CustomWebApplicationFactory<TStartup>>>();
+
+                    context.Database.EnsureCreated();
+
+                    try
+                    {
+                        SeedTestData.InitializeTestDb(context);
+                    }
+                    catch (Exception ex)
+                    {
+                        logger.LogError(ex, $"An error occurred seeding the database with test data. Error: {ex.Message}");
+                    }
+                };
+            });
+        }
+
+        public HttpClient GetAnonymousClient()
+        {
+            return CreateClient();
+        }
+    }
+}
---
+++ CustomerManagement.API.IntegrationTests/Setup/SeedTestData.cs
@@ -0,0 +1,33 @@
+﻿using CustomerManagement.Model;
+using CustomerManagement.Db;
+using System;
+
+namespace CustomerManagement.API.IntegrationTests.Setup
+{
+    public static class SeedTestData
+    {
+        public static void InitializeTestDb(CustomerManagementDbContext context)
+        {
+            var customer1Id = Guid.Parse("{5e8b6eb6-f831-4508-8547-ecbb3153d617}");
+            var customer2Id = Guid.Parse("{b151edb6-7323-4129-83f4-6f1f48832bd0}");
+
+            context.Customer.Add(new Customer
+            {
+                CustomerId = customer1Id,
+                FirstName = "Adam",
+                LastName = "Smith",
+                DateOfBirth = Convert.ToDateTime("01/01/1980")
+            });
+
+            context.Customer.Add(new Customer
+            {
+                CustomerId = customer2Id,
+                FirstName = "Paul",
+                LastName = "Newman",
+                DateOfBirth = Convert.ToDateTime("01/01/1970")
+            });
+            context.SaveChanges();
+
+        }
+    }
+}
