cac231b8d100efcd854423ae3bf3d5592615ce53
Ankush Kashyap <ankushkashyap@gmail.com>
2022-02-21 12:28:30 +1100

    added repo

---
+++ CustomerManagement.Db/Repositories/BaseRepository.cs
@@ -0,0 +1,47 @@
+﻿using CustomerManagement.Application.Contracts;
+using CustomerManagement.Db;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CustomerManagement.Repositories
+{
+    public class BaseRepository<T> : IGenericAsyncRepository<T> where T : class
+    {
+        protected readonly CustomerManagementDbContext _dbContext;
+
+        public BaseRepository(CustomerManagementDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task<T> AddAsync(T entity)
+        {
+            await _dbContext.Set<T>().AddAsync(entity);
+            await _dbContext.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task DeleteAsync(T entity)
+        {
+            _dbContext.Set<T>().Remove(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task<IReadOnlyList<T>> GetAllAsync()
+        {
+            return await _dbContext.Set<T>().ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(Guid id)
+        {
+            return await _dbContext.Set<T>().FindAsync(id);
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _dbContext.Entry(entity).State = EntityState.Modified;
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
---
+++ CustomerManagement.Db/Repositories/CustomerRepository.cs
@@ -0,0 +1,25 @@
+﻿using CustomerManagement.Application.Contracts;
+using CustomerManagement.Model;
+using CustomerManagement.Db;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerManagement.Repositories
+{
+    public class CustomerRepository : BaseRepository<Customer>, ICustomerRepository
+    {
+        public CustomerRepository(CustomerManagementDbContext dbContext): base(dbContext)
+        {
+
+        }
+        public async Task<List<Customer>> SearchCustomerByName(string name)
+        {
+            return await _dbContext.Customer.Where(customer => customer.FirstName.Contains(name, StringComparison.OrdinalIgnoreCase)
+            || customer.LastName.Contains(name, StringComparison.OrdinalIgnoreCase)).ToListAsync();
+        }
+    }
+}
